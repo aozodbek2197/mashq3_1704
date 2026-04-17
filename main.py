5b9129dfdb8be9dd093bb56f6dc23d0937ec0275
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-17 12:54:01 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1-mashq
+d = {"a": 1, "b": 2}
+print(d)
+# 2-mashq
+d = {"name": "Ali"}
+print(d["name"])
+# 3-mashq
+d = {}
+d["age"] = 20
+print(d)
+# 4-mashq
+d = {"a": 1, "b": 2}
+print(d.keys())
+# 5-mashq
+print(d.values())
